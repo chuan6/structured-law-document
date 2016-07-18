dfa3fd3259e5971459906050233aa8e3b7fdebef
chuan6 <chuan6.dev@gmail.com>
2016-07-18 22:46:54 +0800

    add functions to recognize nth-item listing
    ...including 章、节、条。

--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -36,6 +36,53 @@
           (recur (rest s) (conj t (first s)))))
       [[head] [head]])))
 
+(def cdigit-map
+  {\零 0 \一 1 \二 2 \三 3 \四 4 \五 5 \六 6 \七 7 \八 8 \九 9})
+
+(def nthten-map
+  {\十 10 \百 100})
+
+(defn chinese-number
+  {:test
+   #(let [f chinese-number]
+      (tt/comprehend-tests
+       (t/is (= 0   (f "零")))
+       (t/is (= 1   (f "一")))
+       (t/is (= 10  (f "十")))
+       (t/is (= 12  (f "十二")))
+       (t/is (= 20  (f "二十行")))
+       (t/is (= 34  (f "三十四")))
+       (t/is (= 567 (f "五百六十七")))
+       (t/is (= 809 (f "八百零九回")))))}
+  [s]
+  (let [c (first s)]
+    (cond (empty? s) 0
+          (= c \十)  (+ 10 (chinese-number (rest s)))
+          (= c \零)  (chinese-number (rest s))
+
+          (contains? cdigit-map c)
+          (let [d (cdigit-map c)
+                n (second s)]
+            (if (contains? nthten-map n)
+              (+ (* d (nthten-map n)) (chinese-number (nthrest s 2)))
+              d))
+
+          :else 0)))
+
+(defn nth-item
+  {:test
+   #(let [f nth-item]
+      (tt/comprehend-tests
+       (t/is (= [1 \章] (f "第一章 总则")))
+       (t/is (= [12 \条] (f "第十二条 ……")))))}
+  [line]
+  (let [[c & cs] line
+        cd-set (set (concat (keys cdigit-map) (keys nthten-map)))]
+    (when (= c \第)
+      (let [[head tail] (split-with cd-set cs)]
+        (when (seq head)
+          [(chinese-number head) (first tail)])))))
+
 (defn use-chinese-paren
   {:test
    #(let [f use-chinese-paren]
