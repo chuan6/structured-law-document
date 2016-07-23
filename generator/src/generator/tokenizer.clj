a4d494850abe3185b021c65ff130de567757b60a
chuan6 <chuan6.dev@gmail.com>
2016-07-23 15:43:35 +0800

    functions to recognize and reconstruct item string
    ...such as "本法第三十九条和第四十条第一项、第二项".

--- generator/src/generator/tokenizer.clj
+++ generator/src/generator/tokenizer.clj
@@ -1,6 +1,8 @@
 (ns generator.tokenizer
-  (:require [clojure.string :as str]
+  (:require [clojure.core.match :refer [match]]
+            [clojure.string :as str]
             [clojure.test :as t]
+            [clojure.zip :as z]
             [generator.test :as tt]))
 
 (def cdigit-map
@@ -108,3 +110,101 @@
       (when-let [i (chinese-number i-zh)]
         {:token \项 :nth i :head (str/join (-> [\（] (into i-zh) (conj \）)))
          :text line}))))
+
+(def txts ["本法第三十九条和第四十条第一项、第二项"
+           "本法第三十九条和第四十条第一项、第二项"
+           "本法第二十二条和第二十三条"
+           "本法第二十六条第一款"
+           "本法第二十六条第一款第一项"
+           "本条第一款"
+           "本法第四十条、第四十一条"
+           "本法第四十二条"
+           "本法第四十二条第二项"
+           "本法第三十八条"
+           "本法第三十六条"
+           "本法第四十条"
+           "本法第四十一条第一款"
+           "本法第四十四条第一项"
+           "本法第四十四条第四项、第五项"
+           "本法第八十七条"
+           "本法第十七条"
+           "本法第三十六条、第三十八条"
+           "本法第三十九条和第四十条第一项、第二项"
+           "本法第二十六条"
+           "本法第四十七条"
+           "本法第十四条 第二款第三项"
+           "本法第四十六条"])
+
+;(clojure.pprint/pprint (map #(str/split % #"[法条款项和、]") txts))
+
+(def item-char-set (set (seq "法条款项本第一二三四五六七八九十百")))
+
+(defn- left-most-item [s]
+  (loop [xs []
+         ys s]
+    (if (item-char-set (first ys))
+      (let [xs' (conj xs (first ys))
+            ys' (rest ys)]
+        (if (#{"法" "条" "款" "项"} (str (first ys)))
+          [xs' ys']
+          (recur xs' ys')))
+      [[] s])))
+
+(defn- read-items [char-seq]
+  (loop [cs char-seq
+        ts []]
+   (let [c (first cs)]
+     (case c
+       nil [ts []]
+       \和 (recur (rest cs) (conj ts {:token :and :text "和"}))
+       \、 (recur (rest cs) (conj ts {:token :and :text "、"}))
+       (let [[item rest-cs] (left-most-item cs)]
+         (cond (empty? item)
+               [ts rest-cs]
+
+               (= c \本)
+               (recur rest-cs (conj ts {:token (peek item) :nth :this
+                                        :text (str/join item)}))
+
+               :else
+               (let [[i unit] (nth-item item)]
+                 (recur rest-cs (conj ts {:token unit :nth i
+                                          :text (str/join item)})))))))))
+
+(def parse-tree (partial z/zipper
+                         seq? ;branch?
+                         rest ;children
+                         (fn [x children] ;makenode
+                           (cons (first x) children))))
+
+(defn- parse [cs]
+  (let [[recognized-items rest-cs] (read-items cs)]
+    ;(println "recognized-items:" recognized-items)
+    (reduce
+     (fn [loc x]
+                                        ;(println (z/root loc))
+       (let [curr (:token (if (z/branch? loc)
+                            (first (z/node loc))
+                            (z/node loc)))]
+                                        ;(println "node:" (z/node loc))
+         (match [curr (:token x)]
+           [\条 \款] (-> loc (z/insert-child (list x)) z/down)
+           [\条 \项] (-> loc (z/insert-child (list {:token \款 :nth 1})) z/down
+                         (z/insert-child (list x)) z/down)
+           [\条 \条] (-> loc (z/insert-right (list x)) z/right)
+           [\款 \项] (-> loc (z/insert-child (list x)) z/down)
+           [\款 \款] (-> loc (z/insert-right (list x)) z/right)
+           [\款 \条] (-> loc z/up (z/insert-right (list x)) z/right)
+           [\项 \项] (-> loc (z/insert-right (list x)) z/right)
+           [\项 \款] (-> loc z/up (z/insert-right (list x)) z/right)
+           [\项 \条] (-> loc z/up z/up (z/insert-right (list x)) z/right)
+           [\法 \条] (-> loc (z/insert-child (list x)) z/down)
+           [_ :and] (-> loc (z/insert-child x))
+           :else (do (println "unrecognized pattern" [curr x])
+                     loc))))
+     (parse-tree (list (first recognized-items)))
+     (rest recognized-items))))
+
+(let [src "本法第三十九条和第四十条第一项、第二项"
+      r (parse src)]
+  (t/is (= src (str/join (map :text (flatten (z/root r)))))))
