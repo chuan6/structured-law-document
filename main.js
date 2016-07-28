d398a4f06500fc87956f502740c0ffea134015e9
chuan6 <chuan6.dev@gmail.com>
2016-07-28 16:05:15 +0800

    switch to single column layout when the window width is not enough
    ...to fix two columns.

--- main.js
+++ main.js
@@ -8,13 +8,49 @@ function getAvailHeightFn (scrollbarHeight) {
     };
 }
 
+function horizontalSpan(elmt) {
+    var computed = window.getComputedStyle(elmt);
+    return parseFloat(computed.marginLeft)
+        + elmt.offsetWidth
+        + parseFloat(computed.marginRight);
+}
+
+function multiCols (container, h) {
+    container.style.height = px (h ());
+    container.style.columnWidth =
+        container.style.MozColumnWidth =
+        container.style.WebkitColumnWidth = px (300);
+    container.style.columnGap =
+        container.style.MozColumnGap =
+        container.style.WebkitColumnGap = 0;
+}
+
+function singleCol (container) {
+    container.style.height = "auto";
+    container.style.columnWidth =
+        container.style.MozColumnWidth =
+        container.style.WebkitColumnWidth = "normal";
+    container.style.columnGap =
+        container.style.MozColumnGap =
+        container.style.WebkitColumnGap = "normal";
+}
+
+function dynamicLayout (container, h, entry) {
+    if (window.innerWidth >= horizontalSpan (entry) * 2) {
+        multiCols (container, h);
+    } else {
+        singleCol (container);
+    }
+}
+
 window.addEventListener("load", function () {
     var container = document.getElementById("entries-container");
+    var entries = document.getElementsByClassName ("entry");
     var h = getAvailHeightFn(32);
 
-    container.style.height = px(h());
+    dynamicLayout (container, h, entries [0]);
 
     window.onresize = function () {
-        container.style.height = px(h());
+        dynamicLayout (container, h, entries [0]);
     };
 });
