9994723abb130c0f505bb51e31f5d9b0c4204e39
chuan6 <chuan6.dev@gmail.com>
2016-08-09 15:18:00 +0800

    simplify line tokenizing process

--- generator/src/generator/line.clj
+++ generator/src/generator/line.clj
@@ -9,55 +9,16 @@
   {:test
    #(let [f nth-item]
       (tt/comprehend-tests
-       (t/is (= [1 \章] (f "第一章 总则")))
-       (t/is (= [12 \条] (f "第十二条 ……")))))}
+       (t/is (= [1 :章 (seq "第一章")] (f "第一章 总则")))
+       (t/is (= [12 :条 (seq "第十二条")] (f "第十二条 ……")))))}
   [line]
   (let [[c & cs] line]
     (when (= c \第)
-      (let [[i processed] (数字 cs)]
-        [i (first (s/without-prefix cs processed))]))))
-
-(defn nth-章节
-  {:test
-   #(let [f nth-章节]
-      (tt/comprehend-tests
-       (t/is (= {:token \章 :nth 1 :text "第一章 总则"}
-                (f ["第一章 总则" "第一条 ……"])))
-       (t/is (= {:token \节 :nth 2 :text "第二节 劳务派遣"}
-                (f ["第二节 劳务派遣" "第五十七条 ……"])))))}
-  [lines]
-  (let [line (first lines)
-        [i unit] (nth-item line)]
-    (assert (and i (#{\章 \节} unit)))
-    {:token unit :nth i :text line}))
-
-(defn nth-条
-  {:test
-   #(let [f nth-条
-          a ["第五条 县级以上……"]
-          b ["第六条 工会应当……" "……"]
-          c ["第二章 ……"]
-          d ["第七条 用人单位……"]
-          ab (into a b)
-          bcd (-> b (into c) (into d))]
-      (tt/comprehend-tests
-       (t/is (= [a {:token \条 :nth 5 :text a :head "第五条"}]
-                (f ab)))
-       (t/is (= [b {:token \条 :nth 6 :text b :head "第六条"}]
-                (f bcd)))))}
-  [lines]
-  (let [line (first lines)
-        [i unit] (nth-item line)]
-    (assert (and i (= unit \条)))
-    (let [lines-within
-          (into [line] (take-while
-                        (fn [l]
-                          (let [[_ unit] (nth-item l)]
-                            (nil? (#{\章 \节 \条} unit))))
-                        (rest lines)))]
-      [lines-within
-       {:token \条 :nth i :head (first (str/split line #"\s"))
-        :text lines-within}])))
+      (let [[i processed] (数字 cs)
+            tailc (first (s/without-prefix cs processed))]
+        [i
+         (keyword (str tailc))
+         (-> [c] (into processed) (conj tailc))]))))
 
 (defn 括号数字
   {:test
@@ -75,14 +36,95 @@
         (when (and (seq processed) (= processed body))
           [i (s/flatten-and-vector begin body end)])))))
 
-(defn nth-项
+(def table-of-contents-sentinel #"目\s*录")
+(defn table-of-contents
   {:test
-   #(let [f nth-项]
+   #(let [txt ["目 录" "第一章" "第二章" "第三章" "第一章" "……"]]
       (tt/comprehend-tests
-       (t/is (= nil (f "（")))
-       (t/is (= nil (f "（括号内")))
-       (t/is (= {:token \项 :nth 5 :head "（五）" :text "（五）……"}
-                (f "（五）……")))))}
-  [line]
-  (when-let [[i processed] (括号数字 line)]
-    {:token \项 :nth i :head (str/join processed) :text line}))
+       [(t/is (= [["目 录"]
+                  {:token :table-of-contents
+                   :list ["目 录"]}]
+                 (table-of-contents ["目 录"])))
+        (t/is (= [["目 录" "第一章" "第二章" "第三章"]
+                  {:token :table-of-contents
+                   :list ["目 录" "第一章" "第二章" "第三章"]}]
+                 (table-of-contents txt)))]))}
+  [ls]
+  (let [head (first ls)]
+    (assert (re-matches table-of-contents-sentinel head))
+    (if-let [first-item (second ls)]
+      (loop [s (rest (rest ls))
+             t [head first-item]]
+        (if (or (= (first s) first-item) (empty? s))
+          [t {:token :table-of-contents :list t}]
+          (recur (rest s) (conj t (first s)))))
+      [[head] {:token :table-of-contents :list [head]}])))
+
+(defn recognize-table-of-contents
+  {:test
+   #(let [f recognize-table-of-contents
+          txt ["前言" "目 录" "第一章" "第二章" "第三章" "第一章" "……"]]
+      (tt/comprehend-tests
+       [(t/is (= [[] []] (f ["第一章"])))
+        (t/is (= [[{:token :table-of-contents :list ["目 录"]}] []]
+                 (f ["目 录"])))
+        (t/is (= [[{:token :to-be-recognized :text "前言"}
+                   {:token :table-of-contents
+                    :list ["目 录" "第一章" "第二章" "第三章"]}]
+                  ["第一章" "……"]]
+                 (f txt)))]))}
+  ([ls] (recognize-table-of-contents ls []))
+  ([ls ts]
+   (if (empty? ls)
+     [[] []]
+     (let [l (first ls)]
+       (if (re-matches table-of-contents-sentinel l)
+         (let [[p t] (table-of-contents ls)]
+           [(conj ts t) (s/without-prefix ls p)])
+         (recur (rest ls)
+                (conj ts {:token :to-be-recognized :text l})))))))
+
+(defn draw-skeleton
+  {:test
+   #(let [f draw-skeleton
+          lines ["第一章 abc" "第一条 cde" "fgh" "（一）ijk"
+                 "（二）lmn" "opq" "第二条 rst"]
+          r (f lines)]
+      (tt/comprehend-tests
+       (t/is (= {:token :章 :nth 1 :text "第一章 abc"} (nth r 0)))
+       (t/is (= {:token :条 :nth 1 :text "第一条"}     (nth r 1)))
+       (t/is (= {:token :款 :nth 1 :text "cde"}        (nth r 2)))
+       (t/is (= {:token :款 :nth 2 :text "fgh"}        (nth r 3)))
+       (t/is (= {:token :项 :nth 1 :text "（一）ijk"}  (nth r 4)))
+       (t/is (= {:token :项 :nth 2 :text "（二）lmn"}  (nth r 5)))
+       (t/is (= {:token :款 :nth 3 :text "opq"}        (nth r 6)))
+       (t/is (= {:token :条 :nth 2 :text "第二条"}     (nth r 7)))
+       (t/is (= {:token :款 :nth 1 :text "rst"}        (nth r 8)))))}
+  [lines]
+  (let [new-ret (fn [ret t l & i]
+                  (cond-> (-> ret
+                              (update :lines conj t)
+                              (assoc-in [:env :level] l))
+                    (first i) (assoc-in [:env :i-款] (first i))))]
+   (map
+    #(update % :text str/join)
+    (:lines
+     (reduce
+      (fn [{env :env :as ret} line]
+        (if-let [[i unit processed] (nth-item line)]
+          (cond (#{:章 :节} unit)
+                (new-ret ret {:token unit :nth i :text line} unit)
+
+                (= unit :条)
+                (recur
+                 (new-ret ret {:token unit :nth i :text processed} :款 0)
+                 (rest (s/without-prefix line processed))))
+          (apply new-ret ret
+                 (if (#{:款 :项} (:level env))
+                   (if-let [[i _] (括号数字 line)]
+                     [{:token :项 :nth i :text line} :项]
+                     (let [i (inc (:i-款 env))]
+                       [{:token :款 :nth i :text line} :款 i]))
+                   [{:token :to-be-recognized :text line} nil]))))
+      {:env {:level nil :i-款 nil} :lines []}
+      lines)))))
