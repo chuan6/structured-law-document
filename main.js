b8d1b4c9acb976ed1ade08573ced51a898dee71b
chuan6 <chuan6.dev@gmail.com>
2016-08-16 10:54:54 +0800

    remove multicol related code
    ...and leave several functions that though not currently being used,
    might be useful, in the code.

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ function px(x) {
     return "" + x + "px";
 }
 
-function getAvailHeightFn (scrollbarHeight) {
-    return function () {
-        return window.innerHeight - scrollbarHeight;
-    };
-}
-
 function horizontalExtra(computed) {
     var x =  parseFloat(computed.marginLeft)
         + parseFloat(computed.marginRight)
@@ -17,65 +11,6 @@ function horizontalExtra(computed) {
         + parseFloat(computed.paddingRight);
     return x;
 }
-
-function multiCols (container) {
-    var i;
-
-    container.style.columnWidth =
-        container.style.MozColumnWidth =
-        container.style.WebkitColumnWidth = px(300);
-    container.style.columnGap =
-        container.style.MozColumnGap =
-        container.style.WebkitColumnGap = 0;
-}
-
-function singleCol (container) {
-    var i, w;
-
-    container.style.columnWidth =
-        container.style.MozColumnWidth =
-        container.style.WebkitColumnWidth = "normal";
-    container.style.columnGap =
-        container.style.MozColumnGap =
-        container.style.WebkitColumnGap = "normal";
-}
-
-function dynamicLayoutFn(container, es) {
-    var isSingleCol;
-    var h = getAvailHeightFn(32);
-
-    return function () {
-        var computed, minEntryWidth, shouldBeSingleCol;
-
-        computed = window.getComputedStyle(es[0]);
-        minEntryWidth = parseFloat(computed.minWidth) + horizontalExtra(computed);
-        shouldBeSingleCol = true; //(window.innerWidth < minEntryWidth * 2);
-        if (shouldBeSingleCol !== isSingleCol) {
-            if (shouldBeSingleCol) {
-                container.style.height = "auto";
-                singleCol(container);
-            } else {
-                container.style.height = px(h());
-                multiCols(container);
-            }
-            isSingleCol = shouldBeSingleCol;
-        } else if (!shouldBeSingleCol) {
-            container.style.height = px(h());
-        }
-    };
-}
-
-window.addEventListener("load", function () {
-    var container = document.getElementById("entries-container");
-    var entries = document.getElementsByClassName ("entry");
-    var layout = dynamicLayoutFn(container, entries);
-
-    layout();
-
-    window.onresize = function () {
-        layout();
-    };
-});
 
 function getEnclosingID(elmt) {
     return elmt.id? elmt.id : getEnclosingID(elmt.parentNode);
