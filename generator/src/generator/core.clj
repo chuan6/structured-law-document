e6369c2bd48f81af3e775c40b42b6e6980ff3dce
chuan6 <chuan6.dev@gmail.com>
2016-10-13 13:08:22 +0800

    seo: remove "logo" from structured data
    ...since it is not relevant to "WebSite" @type.

--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -189,8 +189,7 @@
      {"@context" "http://schema.org"
       "@type"    "WebSite"
       "name"     "读法.com"
-      "url"      "https://读法.com"
-      "logo"     "https://读法.com/favicon.png"})]])
+      "url"      "https://读法.com"})]])
 
 (defn- add-html-class
   {:test
