4a1db6a61656a447bf84780072b0256a7f831274
chuan6 <chuan6.dev@gmail.com>
2016-10-18 10:22:16 +0800

    add new module: generator.punct for punctuation management

--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -7,6 +7,7 @@
             [generator.id :as id]
             [generator.line :as ln]
             [generator.lisp :as s]
+            [generator.punct :as punct]
             [generator.test :as tt]
             [generator.tokenizer :as tk]
             [generator.parse-tree :as pt]
@@ -16,19 +17,6 @@
   (:gen-class))
 
 (defn default-fn [l] [:p l])
-
-(defn use-chinese-paren
-  {:test
-   #(let [f use-chinese-paren]
-      (tt/comprehend-tests
-       [(t/is (= "（）"     (f "()")))
-        (t/is (= "（六）"   (f "(六)")))
-        (t/is (= "六）"     (f "六)")))
-        (t/is (= "六）（七" (f "六)（七")))]))}
-  [s]
-  (str/replace s #"[\(\)]" #(case %1
-                              "(" "（"
-                              ")" "）")))
 
 (defn space-clapsed
   {:test
@@ -325,7 +313,7 @@
        (with-open [r (io/reader (io/file (io/resource in)))]
          (->> (line-seq r)
               (remove str/blank?)
-              (map (comp use-chinese-paren space-clapsed str/trim))
+              (map (comp punct/use-chinese space-clapsed str/trim))
               (tokenized-lines n)
               (wrap-in-html n l)
               (spit (str "../" out))))))
---
+++ generator/src/generator/punct.clj
@@ -0,0 +1,34 @@
+(ns generator.punct
+  (:require [clojure.string :as str]
+            [clojure.test :as t]
+            [generator.test :as tt]))
+
+(def symbol->label {\: :colon
+                    \： :colon
+                    \; :semi-colon
+                    \； :semi-colon
+                    \, :comma
+                    \， :comma
+                    \. :period
+                    \。 :period})
+
+(defn use-chinese
+  {:test
+   #(let [f use-chinese]
+      (tt/comprehend-tests
+       [(t/is (= "（）"         (f "()")))
+        (t/is (= "（六）"       (f "(六)")))
+        (t/is (= "六）"         (f "六)")))
+        (t/is (= "六）（七"     (f "六)（七")))
+        (t/is (= "组成："       (f "组成:")))
+        (t/is (= "（的）组成：" (f "（的)组成:")))]))}
+  [s]
+  (let [f {":" "："
+           ";" "；"
+           "(" "（"
+           ")" "）"
+           "," "，"}]
+    (str/replace s #"[\:\;\(\)\,]" #(f %1))))
+
+(defn get-seq [s]
+  (remove nil? (map symbol->label s)))
