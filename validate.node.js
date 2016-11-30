49aa9924374632080c0040547b00b9c2cf7561bf
chuan6 <chuan6.dev@gmail.com>
2016-12-01 00:02:47 +0800

    validate.node.js: move code out of callbacks
    ...and into "normal" global functions

--- validate.node.js
+++ validate.node.js
@@ -62,50 +62,48 @@ function norm(s) {
   return cv.join('');
 }
 
-function getReady(href, name) {
-  var mainNode = function (w) {
-    return w.document.querySelector('.entries-container');
-  };
-  var inOriginal = withClassPred('not-in-original-text');
-
-  dom.env({
-    file: href,
-    done: function (err, window) {
-      var fromHTML;
-
-      if (err) throw err;
-
-      fromHTML = norm(domText(mainNode(window), inOriginal));
-
-      fs.readFile(
-        'generator/resources/' + name + '.txt',
-        'utf8',
-        function (err, data) {
-          var fromTXT, isEqual;
-
-          if (err) console.log(err);
-
-          fromTXT = norm(data);
-          isEqual = (fromTXT === fromHTML);
-          if (isEqual) {
-            console.log('pass', name);
-          } else {
-            console.error('fail', name);
-          }
-        }
-      );
-    }
-  });
+function getTextFromHTML(w) {
+  var mainNode = w.document.querySelector('.entries-container'),
+      ignore = withClassPred('not-in-original-text');
+  return domText(mainNode, ignore);
 }
 
-dom.env('index.html', function (err, window) {
-  var entries, e, i;
-
-  if (err) throw err;
-
-  entries = window.document.querySelectorAll('.entry a');
-  for (i = 0; i < entries.length; i++) {
-    e = entries[i];
-    getReady(e.getAttribute('href'), e.textContent);
+function normCmp(a, b, name) {
+  if (norm(a) === norm(b)) {
+    console.log('pass', name);
+  } else {
+    console.error('fail', name);
   }
-});
+};
+
+function makeCallback(f, y, z) {
+  return function (err, x) {
+    if (err) throw err;
+    f(x, y, z);
+  };
+}
+
+function readTXTAndCompare(w, name) {
+  var fromHTML = getTextFromHTML(w);
+
+  fs.readFile(
+    'generator/resources/' + name + '.txt',
+    'utf8',
+    makeCallback(normCmp, fromHTML, name)
+  );
+}
+
+function goThroughRefs(w) {
+  var entries, ref, i;
+
+  entries = w.document.querySelectorAll('.entry a');
+  for (i = 0; i < entries.length; i++) {
+    ref = entries[i];
+    dom.env({
+      file: ref.getAttribute('href'),
+      done: makeCallback(readTXTAndCompare, ref.textContent)
+    });
+  }
+}
+
+dom.env('index.html', makeCallback(goThroughRefs));
