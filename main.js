880d01f8cc7907b8956c7e23b97afcd1660acaa4
chuan6 <chuan6.dev@gmail.com>
2017-02-12 02:25:32 +0800

    minor fix to previous commit
    ...prevent exception from being thrown when the element doesn't
    have a classList property, e.g., when it is of nodeType 3.

--- main.js
+++ main.js
@@ -84,10 +84,14 @@ function textContent(x) {
   var cs;
   var niot = "n-i-o-t";
 
-  if (x.classList.contains(niot)) return "";
-  if (x.nodeType === 3) return x.textContent;
+  if (x.nodeType === 3) {
+    return x.textContent;
+  }
+  if (x.classList && x.classList.contains(niot)) {
+    return "";
+  }
   if (x.tagName === "H2" || x.tagName === "P" || x.tagName === "DIV") {
-    return x.textContent + "↝";
+    return x.textContent + "⏎";
   }
   if (x.tagName === "IMG") {
     return x.alt;
@@ -150,8 +154,8 @@ function shareButtonClosure(elmt) {
   var name, text, link;
 
   var trimEndingBar = function (txt) {
-    // assume that "↝" would never be in original text
-    var bar = "↝", n = bar.length;
+    // assume that "⏎" would never be in original text
+    var bar = "⏎", n = bar.length;
     return txt.endsWith(bar) ? txt.slice(0, txt.length - n) : txt;
   };
 
